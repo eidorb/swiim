00105e451e7b0a9376aff12b3c08fd36f75647df
Brodie Blackburn <brodie.blackburn@gmail.com>
2012-02-18 15:46:56 +1000

    Moved button highlight hiding to disconnected state entry

--- wiiuse.py
+++ wiiuse.py
@@ -1,8 +1,24 @@
 import ctypes
+from ctypes import Structure, c_void_p, c_int, c_byte, c_char, c_float
 import logging
 import os
 
 logger = logging.getLogger('swiim.wiiuse')
+
+class Event(object):
+    WIIUSE_NONE = 0
+    WIIUSE_EVENT = 1
+    WIIUSE_STATUS = 2
+    WIIUSE_CONNECT = 3
+    WIIUSE_DISCONNECT = 4
+    WIIUSE_UNEXPECTED_DISCONNECT = 5
+    WIIUSE_READ_DATA = 6
+    WIIUSE_NUNCHUK_INSERTED = 7
+    WIIUSE_NUNCHUK_REMOVED = 8
+    WIIUSE_CLASSIC_CTRL_INSERTED = 9
+    WIIUSE_CLASSIC_CTRL_REMOVED = 10
+    WIIUSE_GUITAR_HERO_3_CTRL_INSERTED = 11
+    WIIUSE_GUITAR_HERO_3_CTRL_REMOVED = 12
 
 class Wiimote(object):
     LED_NONE = 0x00
@@ -36,12 +52,32 @@ class Wiimote(object):
             dll_path = os.path.join(dll_dir, 'libwiiuse.so')
         logger.info('Using wiiuse library "%s"', dll_path)
         self.dll = ctypes.cdll.LoadLibrary(dll_path)
+        # Initialise an array of wiimotes
         self._wiimotes = self.dll.wiiuse_init(1)
+        self.disconnect = False
 
     def connect(self):
+        """Attempt connection to a Wiimote, timeout after 5 seconds.
+        Return the number of Wiimotes successfully connected to. In this case,
+        it will 1 or 0."""
+
         found = self.dll.wiiuse_find(self._wiimotes, 1, 5)
+        logger.debug('Found %s remote(s)', found)
         if found:
-            return self.dll.wiiuse_connect(self._wiimotes, 1)
+            connected = self.dll.wiiuse_connect(self._wiimotes, 1)
+            logger.debug('Connected to %s remote(s)', connected)
+            return connected
+
+    def poll(self):
+        logger.debug('Entering poll loop')
+        while True:
+            if not self.disconnect:
+                event = self.dll.wiiuse_poll(self._wiimotes, 1)
+                if event != Event.WIIUSE_NONE:
+                    print 'some sort of event'
+            else:
+                logger.debug('Breaking from poll loop')
+                break
 
     def set_leds(self, led_state):
         self.dll.wiiuse_set_leds()
