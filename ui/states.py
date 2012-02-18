192af5c84260a2088d510efc3dd4e8f2c07f1acd
Brodie Blackburn <brodie.blackburn@gmail.com>
2012-02-18 20:22:50 +1000

    Added connect state signals and implemented poll in separate thread

--- ui/states.py
+++ ui/states.py
@@ -1,33 +1,39 @@
 import logging
+import threading
 from PySide import QtCore, QtGui
 from wiiuse import Wiimote
 
 logger = logging.getLogger('swiim.ui.states')
 
-class Controller(object):
+class Controller(QtCore.QObject):
+    connect_succeeded = QtCore.Signal()
+    connect_failed = QtCore.Signal()
+
     def __init__(self, view):
+        super(Controller, self).__init__()
         logger.debug('Initialising controller')
         self.view = view
         self.state_machine = QtCore.QStateMachine()
         # States
         disconnected_state = QtCore.QState(self.state_machine)
-        disconnected_state.entered.connect(self.disconnected_entered)
-
         communication_state = QtCore.QState(self.state_machine)
-        communication_state.entered.connect(self.communication_entered)
-
         connect_state = QtCore.QState(communication_state)
-        connect_state.entered.connect(self.connect_entered)
-
         connected_state = QtCore.QState(communication_state)
-        connected_state.entered.connect(self.connected_entered)
-
         self.state_machine.setInitialState(disconnected_state)
         communication_state.setInitialState(connect_state)
+
+        disconnected_state.entered.connect(self.disconnected_entered)
+        communication_state.entered.connect(self.communication_entered)
+        connect_state.entered.connect(self.connect_entered)
+        connected_state.entered.connect(self.connected_entered)
+        connected_state.exited.connect(self.connected_exited)
 
         # Transitions
         disconnected_state.addTransition(
             view.ui.connect.clicked, communication_state)
+        connect_state.addTransition(self.connect_succeeded, connected_state)
+        connect_state.addTransition(self.connect_failed, disconnected_state)
+        communication_state.addTransition(view.ui.disconnect.clicked, disconnected_state)
 
         self.state_machine.start()
 
@@ -48,12 +54,15 @@ class Controller(object):
         logger.debug('Entered communication state')
         # Disable connect button
         self.view.ui.connect.setEnabled(False)
+        self.wiimote = Wiimote()
 
     def connect_entered(self):
         logger.debug('Entered connect state')
         self.view.set_permanent_message('Connecting to Wiimote')
-        self.wiimote = Wiimote()
-        logger.error('%s', wiimote.BUTTON_A)
+        if self.wiimote.connect():
+            self.connect_succeeded.emit()
+        else:
+            self.connect_failed.emit()
 
     def connected_entered(self):
         logger.debug('Entered connected state')
@@ -62,3 +71,12 @@ class Controller(object):
         self.view.ui.disconnect.setEnabled(True)
         # Enable Wiimote control
         self.view.ui.wiimote.setEnabled(True)
+
+        self.poll_thread = threading.Thread(target=self.wiimote.poll)
+        self.poll_thread.start()
+
+    def connected_exited(self):
+        self.wiimote.disconnect = True
+        logger.debug('Waiting for poll thread to end')
+        self.poll_thread.join()
+        logger.debug('Poll thread ended')
