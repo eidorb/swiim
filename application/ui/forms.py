33e4e34f8e0f6a4452a42c1c853e72a4b64de0b7
Brodie Blackburn <brodie.blackburn@gmail.com>
2012-05-26 16:08:34 +1000

    Add parent=None parameter to custom classes' init

--- application/ui/forms.py
+++ application/ui/forms.py
@@ -3,11 +3,11 @@ import swiim
 from PySide import QtGui
 
 class MainWindow(QtGui.QMainWindow, swiim.Ui_MainWindow):
-    def __init__(self, parent):
+    def __init__(self, parent=None):
         super(MainWindow, self).__init__(parent)
         self.setupUi(self)
 
 class TestForm(QtGui.QWidget, test.Ui_Form):
-    def __init__(self, parent):
+    def __init__(self, parent=None):
         super(TestForm, self).__init__(parent)
         self.setupUi(self)
