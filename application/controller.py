94d2e4e6964e5397ad70087b62eb12fcb3c71e7d
Brodie Blackburn <brodie.blackburn@gmail.com>
2012-05-26 23:13:41 +1000

    Use class-level references to state machine in states

--- application/controller.py
+++ application/controller.py
@@ -8,54 +8,60 @@ log = logging.getLogger('swiim.' + __name__)
 def start_application():
     log.info('Application starting')
     app = view.SwiimApplication()
-    state_machine = StateMachine(app)
+    state_machine = SwiimStateMachine(app)
     log.debug('Starting state machine')
     state_machine.start()
     app.run()
     log.info('Application exiting')
 
-class StateMachine(QtCore.QStateMachine):
+class SwiimStateMachine(QtCore.QStateMachine):
+    """Sets up a reference to the app for convenience."""
     def __init__(self, app):
-        super(StateMachine, self).__init__()
+        super(SwiimStateMachine, self).__init__()
         self.app = app
         self.create_states()
 
     def create_states(self):
         """Create the states that make up the state machine."""
-
-        self.initial = Initial(self)
-        self.test_wiimote = TestWiimote(self)
+        self.initial = Initial(self, self)
+        self.test_wiimote = TestWiimote(self, self)
 
         self.initial.setup_transitions()
 
         self.setInitialState(self.initial)
 
-class Initial(QState):
+class SwiimState(QState):
+    """Sets up references to the state machine and app for convenience."""
+    def __init__(self, parent, state_machine):
+        super(SwiimState, self).__init__(parent)
+        self.state_machine = state_machine
+        self.app = state_machine.app
+
+class Initial(SwiimState):
     def setup_transitions(self):
         self.addTransition(
-            self.machine().app.forms.swiim.actionTestWiimote.triggered,
-            self.machine().test_wiimote)
+            self.app.forms.swiim.actionTestWiimote.triggered,
+            self.state_machine.test_wiimote)
 
     def onEntry(self, event):
         log.debug('Initial state entered')
 
-class TestWiimote(QState):
+class TestWiimote(SwiimState):
     def onEntry(self, event):
         log.debug('Test state entered')
-        self.machine().app.set_permanent_message('Wiimote connection test')
+        self.app.set_permanent_message('Wiimote connection test')
         # Disable test wiimote action
-        self.machine().app.forms.swiim.actionTestWiimote.setEnabled(False)
+        self.app.forms.swiim.actionTestWiimote.setEnabled(False)
         # Set the test wiimote form as the central widget
-        self.machine().app.forms.swiim.setCentralWidget(
-            self.machine().app.forms.wiimote_test
+        self.app.forms.swiim.setCentralWidget(
+            self.app.forms.wiimote_test
         )
 
     def onExit(self, *args, **kwargs):
         # Re-enable test wiimote action
-        self.machine().app.forms.swiim.actionTestWiimote.setEnabled(True)
+        self.app.forms.swiim.actionTestWiimote.setEnabled(True)
 
-
-class Disconnected(QState):
+class Disconnected(SwiimState):
     def onEntry(self, event):
         log.debug('Disconnected state entered')
 
