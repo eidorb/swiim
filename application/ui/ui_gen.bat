ce70343fc7a5e3e4dc7ec067909b5a8f9f354ff4
Brodie Blackburn <brodie.blackburn@gmail.com>
2012-05-26 19:45:46 +1000

    Update wiimote_test reference

--- application/ui/ui_gen.bat
+++ application/ui/ui_gen.bat
@@ -1,4 +1,4 @@
 ..\..\..\swiimenv\Scripts\pyside-uic.exe --output=swiim.py -x -d swiim.ui
 ..\..\..\swiimenv\Scripts\python swiim.py
-..\..\..\swiimenv\Scripts\pyside-uic.exe --output=test.py -x -d test.ui
-..\..\..\swiimenv\Scripts\python test.py
+..\..\..\swiimenv\Scripts\pyside-uic.exe --output=wiimote_test.py -x -d wiimote_test.ui
+..\..\..\swiimenv\Scripts\python wiimote_test.py
