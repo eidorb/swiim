067cd508595e08ae6233036338a51847a79f1ebf
Brodie Blackburn <brodie.blackburn@gmail.com>
2012-07-14 22:11:31 +1000

    Remove ui_gen batch file

--- application/ui/ui_gen.bat
+++
@@ -1,4 +0,0 @@
-..\..\..\swiimenv\Scripts\pyside-uic.exe --output=swiim.py -x -d swiim.ui
-..\..\..\swiimenv\Scripts\python swiim.py
-..\..\..\swiimenv\Scripts\pyside-uic.exe --output=wiimote_test.py -x -d wiimote_test.ui
-..\..\..\swiimenv\Scripts\python wiimote_test.py
